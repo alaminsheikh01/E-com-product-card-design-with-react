ca44784c2ad3b4b99c9a1cd1e5e1cd180947077c
alamin <alamin@gmail.com>
2021-01-02 21:46:33 +0600

    dynamic card design

---
+++ src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Blutooth Headphones",
+    image: "/images/1.jpg",
+    description:
+      "Blutooth techonology lets you connect it with compatible device wirelessly high qulaty acc autio offers immersive listening experience built-in microphone allows you to take calls while working.",
+    brand: "Apply",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Airpods Wireless Blutooth Headphones",
+    image: "/images/2.jpg",
+    description:
+      "Blutooth techonology lets you connect it with compatible device wirelessly high qulaty acc autio offers immersive listening experience built-in microphone allows you to take calls while working.",
+    brand: "Apply",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 3,
+  },
+  {
+    _id: "3",
+    name: "Airpods Wireless Blutooth Headphones",
+    image: "/images/3.jpg",
+    description:
+      "Blutooth techonology lets you connect it with compatible device wirelessly high qulaty acc autio offers immersive listening experience built-in microphone allows you to take calls while working.",
+    brand: "Apply",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: "4",
+    name: "Airpods Wireless Blutooth Headphones",
+    image: "/images/4.jpg",
+    description:
+      "Blutooth techonology lets you connect it with compatible device wirelessly high qulaty acc autio offers immersive listening experience built-in microphone allows you to take calls while working.",
+    brand: "Apply",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: "5",
+    name: "Airpods Wireless Blutooth Headphones",
+    image: "/images/5.jpg",
+    description:
+      "Blutooth techonology lets you connect it with compatible device wirelessly high qulaty acc autio offers immersive listening experience built-in microphone allows you to take calls while working.",
+    brand: "Apply",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 6,
+  },
+  {
+    _id: "6",
+    name: "Airpods Wireless Blutooth Headphones",
+    image: "/images/6.jpg",
+    description:
+      "Blutooth techonology lets you connect it with compatible device wirelessly high qulaty acc autio offers immersive listening experience built-in microphone allows you to take calls while working.",
+    brand: "Apply",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 3.5,
+    numReviews: 5,
+  },
+];
+
+export default products;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import products from "../products";
+import Product from "../components/Product";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Container>
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default HomeScreen;
